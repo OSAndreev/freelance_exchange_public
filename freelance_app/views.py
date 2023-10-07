b0a796ca2c06fab3e914e43e1e950ae09a690005
Admin <admin@Admins-MacBook-Pro.local>
2023-10-07 07:08:22 +0300

    first commit

---
+++ freelance_app/views.py
@@ -0,0 +1,474 @@
+import django_filters
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import CreateView, UpdateView, ListView
+
+from .models import *
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from .forms import *
+
+def start_page(request):
+    return render(request, 'start.html')
+### Никитины
+def freelancer_main_page(request):
+    user_id = get_object_or_404(UserData, django_user_id=get_object_or_404(User, id=request.user.id))
+    freelancer = Freelancer.objects.get(user_id=user_id)
+    offers = Offers.objects.filter(freelance_variety=freelancer)
+    queryset = Order.objects.filter(in_work=0)
+
+    q_list = list(Order.objects.filter(in_work=0).values_list('id', flat=True))
+    of_list = list(Offers.objects.filter(freelance_variety=freelancer).values_list('details', flat=True))
+    took = []
+
+    for i in q_list:
+        if (i in of_list):
+            took.append(True)
+        else:
+            took.append(False)
+
+    balance = check_my_balance(user_id)
+
+    return render(request, 'order_list.html', {'object_list': zip(queryset, took), 'offers_list': offers, 'balance': balance})
+
+def update_deal_history(pk):
+    order = Order.objects.get(id=pk)
+    deal = DealHistory.objects.create(from_who=order.customer.user_id, to_who=order.performer.user_id, order=order)
+    deal.save()
+
+def refuse_offer(request, pk):
+    order = Order.objects.get(pk=pk)
+    Offers.objects.filter(details=order).delete()
+    return redirect('free_orders')
+
+def update_offers(request,pk):
+    order = Order.objects.get(pk=pk)
+    user_id = get_object_or_404(UserData, django_user_id=get_object_or_404(User, id=request.user.id))
+    freelancer = Freelancer.objects.get(user_id=user_id)
+    new_offer = Offers.objects.create(details=order, freelance_variety=freelancer, take=False)
+    new_offer.save()
+    return redirect('free_orders')
+
+def freelancer_my_works(request, pk=''):
+    user_id = get_object_or_404(UserData, django_user_id=get_object_or_404(User, id=request.user.id))
+    freelancer = Freelancer.objects.get(user_id=user_id)
+    queryset = Order.objects.filter(in_work=1, performer=freelancer)
+    form = WorkReadyForm()
+    if request.method == 'POST':
+        form = WorkReadyForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            work_form = cd.get('work_form')
+            degree = cd.get('degree')
+            try:
+                order = Order.objects.get(pk=pk)
+                if order.degree_to_company is None and order.done:
+                    order.degree_to_company = degree
+                else:
+                    order.work_ready = work_form
+                order.save()
+                update_customer_degree(order.customer)
+                return redirect('my_works')
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse('Не валид')
+
+
+    work_ready_list = list(Order.objects.filter(performer=freelancer).values_list('work_ready', flat=True))
+    rejected = []
+
+    for i in work_ready_list:
+        if i != None and " is not accepted." in i:
+            rejected.append(True)
+        else:
+            rejected.append(False)
+    print(rejected)
+    object_list = zip(queryset, rejected)
+    return render(request,'my_works.html', {"object_list": object_list, "form" : form, "balance":
+                                            check_my_balance(user_id)})
+
+def update_customer_degree(customer):
+    all_degrees = list(Order.objects.filter(customer=customer).values_list('degree_to_company', flat=True))
+    print(all_degrees)
+    all_d = []
+    for i in all_degrees:
+        if i != None:
+            all_d.append(i)
+
+    if all_d != []:
+        degree = round(sum(all_d) / len(all_d))
+        customer.degree = degree
+        customer.save()
+#### Никитины
+
+def customer_main_page(request, pk=''):
+    userdata_id = get_object_or_404(UserData, django_user_id=request.user.id)
+    customer_id = get_object_or_404(Customer, user_id=userdata_id)
+    queryset = Order.objects.filter(customer_id=customer_id)
+    form = WorkReadyForm()
+    if request.method == 'POST':
+        form = WorkReadyForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            degree = cd.get('degree')
+            try:
+                order = Order.objects.get(pk=pk)
+                if order.degree_to_performer is None and order.done is True:
+                    order.degree_to_performer = degree
+                    order.save()
+                performer = Order.objects.get(pk=pk)
+                update_performer_degree(performer.performer)
+                return redirect('customer_main_page')
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse('Не валид')
+
+
+
+    work_ready_list = list(Order.objects.filter(customer=customer_id).values_list('work_ready', flat=True))
+    rejected = []
+    print(work_ready_list)
+
+    balance = check_my_balance(customer_id.user_id)
+    for i in work_ready_list:
+        if i != None and " is not accepted." in i:
+            rejected.append(True)
+        else:
+            rejected.append(False)
+
+
+    object_list = zip(queryset, rejected)
+    return render(request, 'customer_main_page.html', {'object_list': object_list, 'form': form, 'balance': balance})
+
+def update_performer_degree(performer):
+    all_degrees = list(Order.objects.filter(performer=performer).values_list('degree_to_performer', flat=True))
+    print(all_degrees)
+    all_d = []
+    for i in all_degrees:
+        if i != None:
+            all_d.append(i)
+
+    if all_d != []:
+        degree = round(sum(all_d) / len(all_d))
+        performer.degree = degree
+        performer.save()
+
+
+def check_my_balance(user_id):
+    adding_operation_list = list(
+        BalanceOperations.objects.filter(user_id=user_id, type_operation=1).values_list('summa', flat=True))
+    print(adding_operation_list)
+    withdraw_operation_list = list(
+        BalanceOperations.objects.filter(user_id=user_id, type_operation=0).values_list('summa', flat=True))
+    if user_id.role == 1:
+        deal_history = list(DealHistory.objects.filter(from_who=user_id).values_list('order'))
+    else:
+        deal_history = list(DealHistory.objects.filter(to_who=user_id).values_list('order'))
+    balance = sum(adding_operation_list)
+
+    if withdraw_operation_list != []:
+        balance = sum(adding_operation_list) - sum(withdraw_operation_list)
+    if deal_history != []:
+        for i in deal_history:
+            order = Order.objects.get(id=i[0])
+            print(order)
+            if user_id.role == 1:
+                balance -= order.cost
+            else:
+                balance += order.cost
+    print(balance)
+    return balance
+
+
+def accept_order(request, pk):
+    order = Order.objects.get(pk=pk)
+    order.done = True
+    order.in_work = 1
+    order.save()
+    update_deal_history(pk)
+    return redirect('my_orders')
+
+
+def refuse_order(request, pk):
+    order = Order.objects.get(pk=pk)
+    if " is not accepted."  not in order.work_ready:
+        order.work_ready = order.work_ready + ' is not accepted.'
+        order.save()
+    return redirect('customer_main_page')
+
+
+def create_order(request): # Отлично работает
+    error = ''
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            created_order = form.save(commit=False)
+            user_id = get_object_or_404(UserData, django_user_id=get_object_or_404(User, id=request.user.id))
+            all_cost =  list(Order.objects.filter(customer=get_object_or_404(Customer, user_id=user_id), done = False).values_list('cost', flat=True))
+            print(all_cost)
+            if created_order.cost <= (check_my_balance(user_id) - sum(all_cost)):
+                created_order.customer = get_object_or_404(Customer, user_id=user_id)
+                created_order.save()
+                return redirect('customer_main_page')
+            else:
+                return redirect('create_order')
+        else:
+            error = 'Форма была неверной'
+
+    form = OrderForm()
+    u_id = get_object_or_404(UserData, django_user_id=get_object_or_404(User, id=request.user.id))
+    balance = check_my_balance(u_id)
+    data = {
+        'form' : form,
+        'error' : error,
+        'balance': balance
+    }
+    return render(request, 'new_order.html', data)
+
+
+def create_user(request):
+    error = ''
+    if request.method == 'POST':
+        form = UserDataForm(request.POST)
+        if form.is_valid():
+            created_user = form.save(commit=False)
+            created_user.django_user_id = get_object_or_404(User, id=request.user.id)
+            created_user.save()
+            user_id = UserData.objects.get(django_user_id=get_object_or_404(User, id=request.user.id))
+            if created_user.role == 1:
+                new_customer = Customer.objects.create(user_id=user_id)
+                new_balance_operation = BalanceOperations.objects.create(user_id=user_id, type_operation=1, summa=0)
+                new_balance_operation.save()
+                new_customer.save()
+                return redirect('customer_main_page')
+            elif created_user.role == 0:
+                new_freelancer = Freelancer.objects.create(user_id=user_id)
+                new_balance_operation = BalanceOperations.objects.create(user_id=user_id, type_operation=1, summa=0)
+                new_balance_operation.save()
+                new_freelancer.save()
+                return redirect('freelancer_main_page')
+        else:
+            error = 'Форма была неверной'
+    form = UserDataForm()
+    data = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'create_user.html', data)
+
+
+class MyprojectLoginView(LoginView):
+    template_name = "login_page.html"
+    form_class = AuthUserForm
+    success_url = reverse_lazy('create_order')
+    def get_success_url(self):
+        logined_user = get_object_or_404(User, id=self.request.user.id)
+        logined_user = get_object_or_404(UserData, django_user_id=logined_user)
+        if logined_user.role == 0:
+            return reverse_lazy('freelancer_main_page')
+        elif logined_user.role == 1:
+            return reverse_lazy('customer_main_page')
+        return self.success_url
+
+class RegisterUserView(CreateView):
+    model = User
+    template_name = 'register_page.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('create_user')
+    success_msg = "Пользователь успешно создан"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = self.object
+        login(self.request, user)  # Авторизация пользователя
+        return response
+
+
+def order_responses(request, order_id):
+    userdata_id = get_object_or_404(UserData, django_user_id=get_object_or_404(User, id=request.user.id))
+    user_offers = Offers.objects.filter(details=get_object_or_404(Order, pk=order_id))
+    data = {'user_offers': user_offers}
+    return render(request, 'order_responses.html', context=data)
+
+def assign_to_order(request, pk):
+    offer = Offers.objects.get(pk=pk)
+    order = offer.details
+
+    # Убираем других назначенных на этот заказ
+    offers = Offers.objects.filter(details=order)
+    for now_offer in offers:
+        now_offer.take = False
+        now_offer.save()
+
+    offer.take = True
+    offer.save()
+    order.in_work = 1
+    order.performer = offer.freelance_variety
+    order.save()
+    return redirect('my_orders')
+
+def remove_from_order(request, pk):
+    offer = Offers.objects.get(pk=pk)
+    order = offer.details
+    offer.take = False
+    offer.save()
+    #Совсем необязательная штука, проверяем, а вдруг мы назначили нескольких
+    offers = Offers.objects.filter(details=order)
+    if not any(now_offer.take for now_offer in offers):
+        order.in_work = 0
+        order.performer = None
+        order.save()
+    return redirect('order_responses', order.pk)
+
+def add_order(request):
+    if request.method == 'POST':
+        form = AddOrderForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('start_page')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
+    else:
+        form = AddOrderForm()
+    return render(request, 'customer_main_page.html', {'form': form})
+
+
+class OrderFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Order
+        fields = ['customer', 'performer', 'cost', 'degree_to_company', 'degree_to_performer', 'id']
+
+class OrderViewAll(ListView):
+    model = Order
+    template_name = 'order_list.html'
+
+
+    def get_queryset(self):
+        filter_set = OrderFilter(self.request.GET, queryset=Order.objects.all())
+        return filter_set.qs
+
+class OrderUpdateView(UpdateView):
+    model = Order
+    form_class = UpdateOrderForm
+    template_name = 'update_order.html'
+    success_url = reverse_lazy('start_page')
+
+
+def my_balance(request):
+    user_id = get_object_or_404(UserData, django_user_id=get_object_or_404(User, id=request.user.id))
+    if request.method == 'POST':
+        form = BalanceForm(request.POST)
+        if form.is_valid():
+            operation = form.save(commit=False)
+
+            operation.user_id = user_id
+            operation.summa = abs(operation.summa)
+
+            if user_id.role == 1:
+                all_cost = list(Order.objects.filter(customer=get_object_or_404(Customer, user_id=user_id),
+                                                    done=False).values_list('cost', flat=True))
+
+                if operation.type_operation == 1 or operation.summa <= (check_my_balance(user_id) - sum(all_cost)):
+                    operation.save()
+                return redirect('my_balance_customer')
+            else:
+                if operation.type_operation == 1 or operation.summa <= check_my_balance(user_id):
+                    operation.save()
+                return redirect('my_balance_freelancer')
+    balance = check_my_balance(user_id)
+    form = BalanceForm()
+    data = {
+        'balance': balance,
+        'form': form,
+        'user_id': user_id
+    }
+    return render(request, 'balance_update.html', context=data)
+
+def my_orders(request, pk=''):
+
+    user_id = get_object_or_404(UserData, django_user_id=get_object_or_404(User, id=request.user.id))
+    customer = get_object_or_404(Customer, user_id=user_id)
+    queryset= Order.objects.filter(customer=customer)
+    form = WorkReadyForm()
+    if request.method == 'POST':
+        form = WorkReadyForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            degree = cd.get('degree')
+            try:
+                order = Order.objects.get(pk=pk)
+                if order.degree_to_performer is None and order.done is True:
+                    order.degree_to_performer = degree
+                    order.save()
+                performer = Order.objects.get(pk=pk)
+                update_performer_degree(performer.performer)
+                return redirect('customer_main_page')
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse('Не валид')
+
+    work_ready_list = list(Order.objects.filter(customer=customer).values_list('work_ready', flat=True))
+
+    rejected = []
+
+
+    for i in work_ready_list:
+        if i != None and " is not accepted." in i:
+            rejected.append(True)
+        else:
+            rejected.append(False)
+    print(rejected)
+    object_list = zip(queryset, rejected)
+
+    return render(request, 'my_orders.html', {'object_list': object_list, 'form': form})
+
+def free_orders(request):
+    user_id = get_object_or_404(UserData, django_user_id=get_object_or_404(User, id=request.user.id))
+    freelancer = Freelancer.objects.get(user_id=user_id)
+    
+    q_list = list(Order.objects.filter(in_work=0).values_list('id', flat=True))
+    of_list = list(Offers.objects.filter(freelance_variety=freelancer).values_list('details', flat=True))
+    try:
+        queryset = Order.objects.filter(in_work=0)
+    except Exception as e:
+        return HttpResponse(f"Error: {str(e)}")
+    took = []
+
+    for i in q_list:
+        if (i in of_list):
+            took.append(True)
+        else:
+            took.append(False)
+
+    return render(request, 'free_orders.html', {'object_list': zip(queryset, took)})
+
+
+def show_deal_history(request):
+    user_id = get_object_or_404(UserData, django_user_id=get_object_or_404(User, id=request.user.id))
+    if user_id.role == 1:
+        d_history = DealHistory.objects.filter(from_who=user_id)
+    else:
+        d_history = DealHistory.objects.filter(to_who=user_id)
+
+    balance_operations = BalanceOperations.objects.filter(user_id=user_id)
+    return render(request, 'deal_history.html', context={'deal_history': d_history,
+                                                         'balance_operations': balance_operations})
+
+def delete_order(request,pk):
+    order = Order.objects.get(id=pk)
+    Offers.objects.filter(details=order).delete()
+    order.delete()
+    return redirect('my_orders')
+
+
+
+
+
+
---
+++ freelance_project/urls.py
@@ -0,0 +1,57 @@
+"""freelance_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+from freelance_app.views import *
+from freelance_app import views
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', start_page, name='start_page'),
+    path('login/', views.MyprojectLoginView.as_view(), name='login_page'),
+    path('register/', views.RegisterUserView.as_view(), name='register_page'),
+    #path('orders/all', OrderListView.as_view()),
+    #path('orders/<int:pk>', OrderRetrieveView.as_view()),
+    path('customer/create_order', create_order, name='create_order'),
+    path('create_user/', create_user, name='create_user'),
+    path('freelancer/main_page', freelancer_main_page, name='freelancer_main_page'),
+    path('customer/customer_main_page', customer_main_page, name='customer_main_page'),
+    path('orders/add', add_order, name='add_order'),
+    path('orders/all', OrderViewAll.as_view(), name='all_orders'),
+    path('orders/update/<int:pk>', OrderUpdateView.as_view(), name='update_order'),
+    re_path(r'customer/customer_main_page/my_orders/order_responses/(?P<order_id>\w+)/$', order_responses, name="order_responses"),
+    re_path(r'customer/customer_main_page/my_orders/order_responses/.*?/(?P<pk>\w+)/$', assign_to_order),
+    re_path(r'customer/customer_main_page/my_orders/order_responses/.*?/(?P<pk>\w+)/remove$', remove_from_order),
+    # Все нижние Никитины
+    path('freelancer/main_page/free_orders/chose/<int:pk>', update_offers),
+    path('freelancer/main_page/my_works', freelancer_my_works, name='my_works'),
+    path('freelancer/main_page/free_orders/refuse/<int:pk>', refuse_offer),
+    path('freelancer/main_page/my_works/save_answer/<int:pk>', freelancer_my_works),
+    path('customer/customer_main_page/my_orders/accept/<int:pk>', accept_order),
+    path('customer/customer_main_page/my_orders/rate/<int:pk>', customer_main_page),
+    path('customer/customer_main_page/my_orders/reject/<int:pk>', refuse_order),
+    #новое
+    path('customer/customer_main_page/my_orders', my_orders, name='my_orders'),
+    path('freelancer/main_page/free_orders', free_orders, name='free_orders'),
+    path('customer/customer_main_page/balance', my_balance, name='my_balance_customer'),
+    path('freelancer/main_page/balance', my_balance, name='my_balance_freelancer'),
+    path('customer/customer_main_page/deal_history', show_deal_history),
+    path('freelancer/main_page/deal_history', show_deal_history),
+    path('customer/customer_main_page/my_orders/delete_order/<int:pk>', delete_order)
+
+]
